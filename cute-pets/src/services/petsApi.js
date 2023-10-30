3832a7edf593ce7a6a0c24faeff36d9ec6cc4ae9
Osama <ossamaghoneim4@gmail.com>
2023-10-30 17:43:28 +0100

    EVOL : create a service to fetch data related to pets

---
+++ cute-pets/src/services/petsApi.js
@@ -0,0 +1,5 @@
+export const pets = [
+    {title: 'Cat', imgUrl: '../assets/cuteCat.jpg', alt: 'cat img', description: 'Cat Description => fsdkjlfhsd flhsdlkfhdghqf fgqdfhqskdjfh slqdfh sdiulfhslk dfhleuzfhklj sdfhldkqjd fhlskdfhsdlf klqsdfh kjldsqsdhflkjsdsdh lkj'},
+    {title: 'Dog', imgUrl: '../assets/cuteDog.jpg', alt: 'dog img', description: 'Dog Description => fsdkjlfhsd flhsdlkfhdghqf fgqdfhqskdjfh slqdfh sdiulfhslk dfhleuzfhklj sdfhldkqjd fhlskdfhsdlf klqsdfh kjldsqsdhflkjsdsdh lkj'},
+    {title: 'Hamester', imgUrl: '../assets/cuteHamester.jpg', alt: 'hamester img', description: 'Hamester Description => fsdkjlfhsd flhsdlkfhdghqf fgqdfhqskdjfh slqdfh sdiulfhslk dfhleuzfhklj sdfhldkqjd fhlskdfhsdlf klqsdfh kjldsqsdhflkjsdsdh lkj'}
+];
