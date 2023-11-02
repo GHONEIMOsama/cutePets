8d1eae7e5374ee755263f3365a08a7530b66498b
Osama <ossamaghoneim4@gmail.com>
2023-11-02 17:27:21 +0100

    STYLE: remove unused code

--- cute-pets/src/services/petsApi.js
+++ cute-pets/src/services/petsApi.js
@@ -1,5 +1,5 @@
 export function fetchAllData() {
-    return fetch('https://api.thedogapi.com/v1/images/search?has_breeds=1&api_key=live_kwHlZtE6b1vHgLuVMbDDx2STtQm44tSJu3pmIJRO2STUJoy3IS7uiab0nzHa36gA&limit=10').then(res => {
+    return fetch('https://api.thedogapi.com/v1/images/search?has_breeds=1&api_key=live_kwHlZtE6b1vHgLuVMbDDx2STtQm44tSJu3pmIJRO2STUJoy3IS7uiab0nzHa36gA&limit=20').then(res => {
         return res.json();
     });
 }
